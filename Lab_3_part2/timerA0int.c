16385904660f7b95c9f3be9f27e38ec8c0713f6c
Nanu Dahal <nanudahal2000@yahoo.com>
2019-10-03 18:17:46 -0600

    hi

--- Lab_3_part2/timerA0int.c
+++ Lab_3_part2/timerA0int.c
@@ -12,14 +12,16 @@ void config_interrupt_timer(void){
     TA0CTL |= 0b1000000000; // SMCLK set 1 in bit 9
     TA0CTL &= ~0b100000000; // SMCLK set 0 in bit 8
     TA0CCTL1 |= 0x0E0; //0b 1110 0000; //set mode 7 (reset set)
-    TA0CTL |= 0b10; //enable timer
+    TA0CTL |= 0b10; //enable timer interrupt
+    NVIC_EnableIRQ(TA0_0_IRQn);
+    //priority
 }
 
 
 void start_interrupt(uint8_t duty_cycle){
-   N
+    TA0CTL |= 0b010000; //up mode
+    TA0CTL &= ~0b100000;//up mode pt 2
 }
-
 
 void stop_interrupt(void){
     TA0CTL &= ~0x30;//0011 0000
