8ff4ae53257a86cd7e185cafa2056d57dc5e6820
Giselle <gisellegk@gmail.com>
2019-10-21 20:47:51 -0600

    move all constants to header file, a bit of cleanup

--- Lab3_Part3/drv2605l.c
+++ Lab3_Part3/drv2605l.c
@@ -8,21 +8,8 @@
 #include "msp.h"
 #include "i2c.h"
 #include "gpio.h"
+#include "drv2605l.h"
 
-#define DRV2605L 0x5A // 7-bit address
-
-#define STATUS 0x00
-#define MODE 0x01
-#define GO 0x0C
-#define CONTROL3 0x1D
-
-#define STANDBY_READY 0x00
-#define  LRA_LIBRARY 0x06;
-
-#define MODE_PWM 0x03
-#define MODE_STANDBY (1<<6)
-#define GO_GO 0x01
-#define LIBRARY 0x3
 
 void config_drv2605l() {
     config_drv_gpio(); //configure EN pin
@@ -32,13 +19,11 @@ void config_drv2605l() {
 
     write_register(DRV2605L, MODE, STANDBY_READY);    //Device Ready
     uint8_t mode_reg = read_register(DRV2605L, MODE); //for debug
+    write_register(DRV2605L, MODE, MODE_PWM); // set MODE register to 3
+    write_register(DRV2605L, CONTROL3, PWM_INPUT);   //  set CONTROL3 to PWM
+    write_register(DRV2605L, LIBRARY, LRA_LIBRARY);  //   select LRA library
+    mode_reg = read_register(DRV2605L, MODE);
 
-    write_register(DRV2605L, MODE, MODE_PWM); //come out of standby and set MODE register to 3
-
-    write_register(DRV2605L, CONTROL3, 0b00);         //  set CONTROL3 to PWM
-    write_register(DRV2605L, LIBRARY, 0x6);           //   select LRA library
-
-mode_reg = read_register(DRV2605L, MODE);
     if(mode_reg != (MODE_PWM)){
         P1->DIR |= BIT0;
         P1->OUT |= BIT0; //Indicates error occurred
--- Lab3_Part3/drv2605l.h
+++ Lab3_Part3/drv2605l.h
@@ -8,9 +8,32 @@
 #ifndef DRV2605L_H_
 #define DRV2605L_H_
 
+//Device address
 #define DRV2605L    0x5A        // 7-bit address (8.5.3.1 note)
-#define MODE        0x01
-#define CONTROL3    0x1D
+
+// register addresses
+#define STATUS 0x00
+#define MODE 0x01
+#define LIBRARY 0x3
+#define GO 0x0C
+#define CONTROL3 0x1D
+
+// reg values
+
+// MODE register
+#define STANDBY_READY 0x00
+#define MODE_PWM 0x03
+#define MODE_STANDBY (1<<6)
+
+// LIBRARY register
+#define  LRA_LIBRARY 0x06
+
+// GO register
+#define GO_GO 0x01
+
+// Control3 Register
+#define PWM_INPUT 0x0
+
 
 void config_drv2605l(void);
 
